00a35cf75cdfae5b89e42808743f3159a2bccbe3
myzhoulang <604389771@qq.com>
2017-08-10 18:44:31 +0800

    集成第三方组件

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,27 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'starter',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt.js project'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
   modules: [
     ['@nuxtjs/proxy']
@@ -24,26 +34,61 @@ module.exports = {
     }
   },
   /*
-  ** Global CSS
-  */
-  css: ['~/assets/css/main.css'],
+   ** Global CSS
+   */
+  css: ['~/assets/css/main.css', 'element-ui/lib/theme-default/index.css'],
   /*
-  ** Add axios globally
-  */
+   ** Add axios globally
+   */
+  plugins: ['~plugins/element-ui'],
   build: {
-    vendor: ['axios'],
-    /*
-    ** Run ESLINT on save
-    */
-    extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
+    vendor: ['axios', 'element-ui'],
+
+    babel: {
+      plugins: [
+        ['component', [{
+          libraryName: 'element-ui',
+          styleLibraryName: 'theme-default'
+        }]]
+      ]
+    },
+
+    loaders: [{
+        test: /\.(png|jpe?g|gif|svg)$/,
+        loader: 'url-loader',
+        query: {
+          limit: 1000,
+          name: 'img/[name].[hash:7].[ext]'
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 1000,
+          name: 'fonts/[name].[hash:7].[ext]'
+        }
       }
+    ],
+    postcss: [
+      require('autoprefixer')({
+        browsers: ['last 3 versions']
+      })
+    ]
+  },
+
+
+  /*
+   ** Run ESLINT on save
+   */
+  extend(config, ctx) {
+    if (ctx.isClient) {
+      config.module.rules.push({
+        enforce: 'pre',
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        exclude: /(node_modules)/
+      })
     }
   }
 }
--- pages/shops/index.vue
+++ pages/shops/index.vue
@@ -1,20 +1,27 @@
 <template id="products">
   <section>
 
-    <router-link to="/">首页</router-link>
-    <!-- <a href=""></a> -->
+    <el-row>
+      <el-col :span="24">
+        <h2>商品列表页面</h2>
+      </el-col>
+    </el-row>
 
-    <!-- <button type="button" name="button" @click="back()">回到首页</button> -->
-    <h2>商品列表页面</h2>
-
-    <ul>
-      <li v-for="(product, index) in products" :key="index">
-        <nuxt-link :to="{ name: 'shops-id', params: { id: product.id }}">
-          <span>{{ product.name }} - {{product.phone}}</span>
-          <time>{{ product.time }}</time>
-        </nuxt-link>
-      </li>
-    </ul>
+    <el-row>
+      <el-col :span="6">
+        <h3>XXX</h3>
+      </el-col>
+      <el-col :span="18">
+        <ul>
+          <li v-for="(product, index) in products" :key="index">
+            <nuxt-link :to="{ name: 'shops-id', params: { id: product.id }}">
+              <span>{{ product.name }} - {{product.phone}}</span>
+              <time>{{ product.time }}</time>
+            </nuxt-link>
+          </li>
+        </ul>
+      </el-col>
+    </el-row>
   </section>
 </template>
 
@@ -23,15 +30,20 @@
   import axios from '~/plugins/axios'
   export default {
     layout: 'products',
+    data() {
+      return {
+        num1: 1
+      };
+    },
+    methods: {
+      handleChange(value) {
+        console.log(value);
+      }
+    },
     // 异步数据获取
     async asyncData (context) {
       try {
-        // let url = `/api/shops`
-        // if (context.isClient) {
-        //   url = `/api/shops`
-        // } else {
         let url = `/restapi/shopping/restaurants?extras%5B%5D=activities&geohash=wtmkps3fh90&latitude=30.25807&limit=24&longitude=120.215&offset=0&terminal=web`
-        // }
         const { data } = await axios.get(url)
         return {
           products: data
